d27a59f15faa20793b8bc7781db1d0e2930070cd
eminyilmazz <58512029+eminyilmazz@users.noreply.github.com>
2020-03-14 01:26:36 +0300

    Update PieBar.java

--- PieBar/PieBar.java
+++ PieBar/PieBar.java
@@ -6,7 +6,7 @@ public class PieBar {
     public static void main(String[] args) throws InterruptedException {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to our pie store!\n");
-        String pieTypes = "01: Strawberry Pie\n02: Blueberry Pie\n03: Pumpkin Strawberry Pie\n04: Apple Pie" +
+        String pieTypes = "01: Strawberry Pie\n02: Blueberry Pie\n03: Pumpkin Pie\n04: Apple Pie" +
                           "\n05: Cherry Pie\n06: Peach Pie\n07: Mixed Berry Pie\n08: Lemon Meringue Pie";
         System.out.println("You can choose your order from the list below!\n" + pieTypes);
         String order = input.nextLine();
